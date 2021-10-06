9a425fa2d13c5c66205304b19cc5e880b0835bbd
Thiago Felipe Dos Santos <thiagonfss@gmail.com>
2021-10-06 00:52:52 -0300

    Alteração da logo, adequação das rotas.

--- src/main.js
+++ src/main.js
@@ -2,5 +2,8 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import VueMeta from 'vue-meta'
 
-createApp(App).use(store).use(router).mount("#app");
+const vue = createApp(App)
+
+vue.use(store).use(router).use(VueMeta).mount("#app");
